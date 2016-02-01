49686b3d3a7ea48a9659595e0f8386788f1c3815
Kay.Meulders <Kay.Meulders@PC243-06.ad.core.vdabantwerpen.intra>
2016-02-01 15:10:26 +0100

    bonjour

--- src/gitcursus/GitCursus.java
+++ src/gitcursus/GitCursus.java
@@ -16,6 +16,8 @@ public class GitCursus {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        
+       System.out.println("Bonjour");
     }
     
 }
